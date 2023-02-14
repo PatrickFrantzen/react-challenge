ba9eac8fe54d600fe13f92fa6d72124503cb3d17
Patrick Frantzen <patrick.frantzen@web.de>
2023-02-14 21:04:26 +0100

    first commit

--- src/App.js
+++ src/App.js
@@ -1,25 +1,86 @@
-import logo from './logo.svg';
+
 import './App.css';
+import { useState, useEffect } from "react";
 
-function App() {
+
+
+export default function App() {
+
+
+
+  const firstPokemonUrl = 'https://pokeapi.co/api/v2/pokemon?limit=10&offset=0';
+  const [count, setCount] = useState(11);
+  const [data, setData] = useState([]);
+  const [isBusy, setBusy] = useState(true)
+  const nexturl = `https://pokeapi.co/api/v2/pokemon/${count}/`;
+  const firstLoad = [];
+  let firstClick = true;
+
+
+  const handeClick = () => {
+    if (firstClick) {
+      firstClick = false;
+      pushToArray()
+      loadMorePokemon()
+      setCount(count + 1);
+      
+    } else {
+      loadMorePokemon()
+      setCount(count + 1);
+    }
+  };
+
+  function pushToArray() {
+    data.forEach(pokemon => {
+      firstLoad.push(pokemon)
+    })
+    setData(firstLoad);
+  }
+
+  async function loadMorePokemon() {
+    const nextPokemonUrl = await fetch(nexturl);
+    const json = await nextPokemonUrl.json();
+    firstLoad.push({ name: json.name });
+  }
+
+
+
+  useEffect(() => {
+    setBusy(false);
+    const fetchData = async () => {
+      const response = await fetch(firstPokemonUrl);
+      const json = await response.json();
+      setData(json.results);
+    }
+    fetchData()
+  }, [])
+
+
+
+  const PokemonList = () => {
+    return (
+      <ul>
+        {data.map((pokemon, index) => {
+          return <li key={index}>{pokemon.name}</li>
+        })}
+      </ul>
+    );
+  }
+
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      {isBusy ? (
+        <h1>Nichts geladen</h1>
+      ) : (
+        <div>
+          <PokemonList />
+          <button onClick={handeClick}>Load More</button>
+        </div>
+      )
+      }
     </div>
-  );
-}
+  )
 
-export default App;
+
+}
